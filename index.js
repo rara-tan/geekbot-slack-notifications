4f7b1b31a543d783ab0075068af9853924189307
rara-tan <barasan1119@gmail.com>
2023-02-26 10:08:42 +0900

    add script code

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const script = require('./script');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  script({
+    geekbotApiKey: process.env.geekbot_api_key,
+    fetch,
+    core,
+    questionIds: process.env.question_ids,
+    standupId, process.env.standup_id,
+    members, process.env.members
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
---
+++ script.js
@@ -0,0 +1,102 @@
+module.exports = async ({ geekbotApiKey, fetch, core, questionIds, standupId, members }) => {
+  const d = new Date()
+  const dateAfter = parseInt(d.setDate(d.getDate() - 7) / 1000)
+
+  const reportResults = await Promise.all(
+    members.map(async (member) => {
+      const userId = member.id
+      const query = new URLSearchParams({
+        question_ids: questionIds,
+        standup_id: standupId,
+        user_id: userId,
+        after: dateAfter,
+      }).toString()
+      try {
+        const res = await fetch(`https://api.geekbot.com/v1/reports?${query}`, {
+          headers: { Authorization: geekbotApiKey },
+        })
+        if (!res.ok) {
+          return {
+            isSuccess: false,
+            userId,
+            message: 'server error',
+          }
+        }
+        let contents
+        try {
+          contents = await res.json()
+        } catch (err) {
+          return {
+            isSuccess: false,
+            userId,
+            message: 'json error',
+          }
+        }
+        return {
+          isSuccess: true,
+          userId,
+          message: 'Succeeded',
+          contents,
+        }
+      } catch (err) {
+        core.error(err)
+        return {
+          isSuccess: false,
+          userId,
+          message: 'network error',
+        }
+      }
+    }),
+  )
+
+  const slackMessages = reportResults.map((report) => {
+    const slackMessage = {
+      mrkdwn_in: ['text'],
+      color: Math.floor(Math.random() * 16777215).toString(16), // random color
+      author_name: members.find(
+        (member) => member.id === report.userId,
+      ).name,
+    }
+
+    // Fetch Error
+    if (!report.isSuccess) {
+      slackMessage.text = `Failed to get Contents. Due to ${report.message}`
+      return slackMessage
+    }
+
+    // No Contents
+    if (report.contents.length === 0) {
+      slackMessage.text = `No Contents`
+      return slackMessage
+    }
+
+    // No Questions
+    const answeredReports = report.contents.filter(
+      (content) => content.questions.length > 0,
+    )
+    if (answeredReports.length === 0) {
+      slackMessage.text = `No Questions`
+      return slackMessage
+    }
+
+    slackMessage.fields = answeredReports.map((report) => {
+      const d = new Date(report.timestamp * 1000)
+      return {
+        title: `【${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}】`,
+        value: `${report.questions[0].answer}`,
+        short: false,
+      }
+    })
+    return slackMessage
+  })
+
+  return {
+    attachments: [
+      {
+        mrkdwn_in: ['text'],
+        text: ':robot_face: Hello! The completed tasks of the team members over the past 1 week are as follows',
+      },
+      ...slackMessages,
+    ],
+  }
+}
