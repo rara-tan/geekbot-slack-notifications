f5f6242cb0fcab639caa6874791e7e3bd4567177
rara-tan <barasan1119@gmail.com>
2023-02-26 10:41:10 +0900

    set async

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const script = require('./script');
 
-try {
+(async () => {
   const questionIds = process.env.question_ids.split(',').trim();
   const standupId = process.env.standup_id;
   const members = process.env.members.split(',').trim();
-  const result = await script({
-    geekbotApiKey: process.env.geekbot_api_key,
-    fetch,
-    core,
-    questionIds,
-    standupId,
-    members
-  });
-  core.setOutput(result);
-} catch (error) {
-  core.setFailed(error.message);
-}
+  try {
+    const result = await script({
+      geekbotApiKey: process.env.geekbot_api_key,
+      fetch,
+      core,
+      questionIds,
+      standupId,
+      members
+    });
+    core.setOutput(result);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
+
