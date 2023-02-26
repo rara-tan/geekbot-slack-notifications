37eedc20d238e37cf63325f8ef9da23f89d259d8
rara-tan <barasan1119@gmail.com>
2023-02-26 11:03:31 +0900

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const script = require('./script');
+const fetch = require('node-fetch');
 
 (async () => {
   const questionIds = core.getInput('question_ids').trim().split(',');
