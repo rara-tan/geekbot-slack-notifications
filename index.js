111555fc4156d2ef367271bd5f86467c5fdccfa6
rara-tan <barasan1119@gmail.com>
2023-02-26 10:30:38 +0900

    change input types

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const github = require('@actions/github');
 const script = require('./script');
 
 try {
+  const questionIds = process.env.question_ids.split(',').trim();
+  const standupId = process.env.standup_id;
+  const members = process.env.members.split(',').trim();
   const result = await script({
     geekbotApiKey: process.env.geekbot_api_key,
     fetch,
     core,
-    questionIds: process.env.question_ids,
-    standupId, process.env.standup_id,
-    members, process.env.members
+    questionIds,
+    standupId,
+    members
   });
   core.setOutput(result);
 } catch (error) {
